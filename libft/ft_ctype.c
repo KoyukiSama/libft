85420ef498cfdf5f245019628d733d7da32daf45
Katarina Claes <kclaes@f1r1s10.codam.nl>
2025-04-22 17:28:33 +0200

    add (ft_ctype.c), ft_ctypelike funcs

---
+++ libft/ft_ctype.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_ctype.c                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/04/22 16:36:29 by kclaes        #+#    #+#                 */
+/*   Updated: 2025/04/22 17:27:20 by kclaes        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+// EOF or unsigned char
+int	ft_isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z'
+		|| c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
+
+int ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+int ft_isalnum(int c)
+{
+	return (ft_isalpha(c) || ft_isdigit(c));
+}
+
+int ft_isascii(int c)
+{
+	if (c >= 0 && c <= 127)
+		return (1);
+	return (0);
+}
+
+int ft_isprint(int c)
+{
+	if (c >= 32 && c <= 126)
+		return (1);
+	return (0);
+}
---
+++ libft/libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   libft.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/04/22 16:57:18 by kclaes        #+#    #+#                 */
+/*   Updated: 2025/04/22 17:28:05 by kclaes        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# define EOF (-1)
+
+int	ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+
+#endif
