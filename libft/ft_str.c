eb3012ae6b8d88b084abe054482da9ce012aae8f
Katarina <kateriiner@cute-huisbaas.localdomain>
2025-04-23 22:06:17 +0200

    add (libft/ft_str.c), ft_strrchr

--- libft/ft_str.c
+++ libft/ft_str.c
@@ -6,7 +6,7 @@
 /*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/23 14:32:58 by kclaes        #+#    #+#                 */
-/*   Updated: 2025/04/23 21:45:38 by kclaes        ########   odam.nl         */
+/*   Updated: 2025/04/23 22:05:44 by kclaes        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,11 +80,11 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 
 char	*ft_strchr(const char *s, int c)
 {
-	unsigned char		uc;
 	const unsigned char	*us;
+	unsigned char		uc;
 
-	uc = (unsigned char) c;
 	us = (const unsigned char *) s;
+	uc = (unsigned char) c;
 	while (*us)
 	{
 		if (*us == uc)
@@ -96,9 +96,34 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
-char *strrchr(const char *s, int c)
+//    The  strchr()  and  strrchr()
+//    functions return a pointer to
+//    the matched character or NULL
+//    if  the  character   is   not
+//    found.   The terminating null
+//    byte is  considered  part  of
+//    the  string,  so that if c is
+//    specified  as   '\0',   these
+//    functions return a pointer to
+//    the terminator.				(from man page strrchr)
+char	*ft_strrchr(const char *s, int c)
 {
-	
+	const unsigned char	*last_ptr;
+	const unsigned char	*us;
+	unsigned char		uc;
+
+	us = (const unsigned char *) s;
+	uc = (unsigned char) c;
+	last_ptr = NULL;
+	while (*us)
+	{
+		if (*us == uc)
+			last_ptr = us;
+		us++;
+	}
+	if (uc == '\0')
+		return ((char *) us);
+	return ((char *) last_ptr);
 }
 
 // strncmp
