6e9a7b1dbb18b461671fd7abce7c0b73a5763db5
Katarina <kateriiner@cute-huisbaas.localdomain>
2025-04-30 13:44:57 +0200

    fix ft_lstmap_bonus.c, freed content with del seperatly

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,33 +6,31 @@
 /*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/28 22:42:26 by kclaes        #+#    #+#                 */
-/*   Updated: 2025/04/29 16:45:00 by kclaes        ########   odam.nl         */
+/*   Updated: 2025/04/30 13:40:26 by kclaes        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_bonus.h"
 
-t_list	*ft_lstclear_ret(t_list **lst, void (*del)(void *));
-
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*lst_new;
 	t_list	*node_new;
+	t_list	*cont_new;
 
 	lst_new = NULL;
 	while (lst)
 	{
-		node_new = ft_lstnew(f(lst->content));
+		cont_new = f(lst->content);
+		node_new = ft_lstnew(cont_new);
 		if (!node_new)
-			return (ft_lstclear_ret(&lst_new, del));
+		{
+			del(cont_new);
+			ft_lstclear(&lst_new, del);
+			return (NULL);
+		}
 		ft_lstadd_back(&lst_new, node_new);
 		lst = lst->next;
 	}
 	return (lst_new);
-}
-
-t_list	*ft_lstclear_ret(t_list **lst, void (*del)(void *))
-{
-	ft_lstclear(lst, del);
-	return (NULL);
 }
