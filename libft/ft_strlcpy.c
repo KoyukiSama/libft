45c66fda503e11b5f2fff9dac2da1158a27494fc
Katarina Claes <kclaes@f0r1s13.codam.nl>
2025-05-03 13:24:46 +0200

    fix norm err

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,13 +6,12 @@
 /*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/23 14:32:58 by kclaes        #+#    #+#                 */
-/*   Updated: 2025/05/03 13:15:39 by kclaes        ########   odam.nl         */
+/*   Updated: 2025/05/03 13:24:35 by kclaes        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stddef.h>
-
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
