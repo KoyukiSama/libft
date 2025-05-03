ad56f63dac68ef9473a09ecb260e69e498deb8d0
Katarina Claes <kclaes@f0r1s13.codam.nl>
2025-05-03 13:26:40 +0200

    fix undefined references

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,10 +6,11 @@
 /*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/28 21:04:57 by kclaes        #+#    #+#                 */
-/*   Updated: 2025/05/03 13:18:36 by kclaes        ########   odam.nl         */
+/*   Updated: 2025/05/03 13:26:08 by kclaes        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <unistd.h>
 
 void	ft_putstr_fd(char *s, int fd)
