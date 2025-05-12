a6010e99723dfc8b9b3ce373052de8b079421e9f
Katarina <kateriiner@cute-huisbaas.localdomain>
2025-05-12 19:04:24 +0200

    add (ft_arrlst_get.c), set up comment functions

---
+++ ft_arrlst_get.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_arrlst_get.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/05/12 18:57:42 by kclaes        #+#    #+#                 */
+/*   Updated: 2025/05/12 19:03:28 by kclaes        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_arrlst.h"
+#include <stdlib.h>
+
+// size_t	ft_arrlst_get_len
+
+// size_t	ft_arrlst_get_cap
+
+// void		*ft_arrlst_get_at_i
+ 
+// size_t	ft_arrlst_get_chri
+
+// size_t	ft_arrlst_get_rchri
+
+// void		*ft_arrlst_get_chr
+
+// void		*ft_arrlst_get_rchr
